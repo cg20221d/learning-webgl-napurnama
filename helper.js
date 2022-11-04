e983781f30b166595e2909c7b5eb319b733f544f
napurnama <purnama.naufal.adli@gmail.com>
2022-11-05 05:51:20 +0700

    add z-index

--- helper.js
+++ helper.js
@@ -3,40 +3,6 @@ function draw(gl, shaderProgram, drawMode, vertices, dimension, subarrayLen){
   gl.drawArrays(drawMode, 0, vertices.length/subarrayLen);
 }
 
-// function drawSevenSegment(number, x, y, length, width){
-//   let segmentArray = drawSevenSegmentHelper(number);
-//   if(segmentArray[0]){
-//     drawSegmentOne(x, y, length, width);
-//   }if(segmentArray[1]){
-//     drawSegmentTwo(x, y, length, width);
-//   }if(segmentArray[2]){
-//     drawSegmentThree(x, y, length, width);
-//   }if(segmentArray[3]){
-//     drawSegmentFour(x, y, length, width);
-//   }if(segmentArray[4]){
-//     drawSegmentFive(x, y, length, width);
-//   }if(segmentArray[5]){
-//     drawSegmentSix(x, y, length, width);
-//   }if(segmentArray[6]){
-//     drawSegmentSeven(x, y, length, width);
-//   }
-// }
-
-// function drawSevenSegmentHelper(number){
-//   let segmentArray = [];
-//   if(number == 0) segmentArray = [1,1,1,1,1,1,0];
-//   if(number == 1) segmentArray = [0,1,1,0,0,0,0];
-//   if(number == 2) segmentArray = [1,1,0,1,1,0,1];
-//   if(number == 3) segmentArray = [1,1,1,1,0,0,1];
-//   if(number == 4) segmentArray = [0,1,1,0,0,1,1];
-//   if(number == 5) segmentArray = [1,0,1,1,0,1,1];
-//   if(number == 6) segmentArray = [1,0,1,1,1,1,1];
-//   if(number == 7) segmentArray = [1,1,1,0,0,0,0];
-//   if(number == 8) segmentArray = [1,1,1,1,1,1,1];
-//   if(number == 9) segmentArray = [1,1,1,1,0,1,1];
-//   return segmentArray;
-// }
-
 function setBuffer(gl, shaderProgram, vertices, dimension, subarrayLen){
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
